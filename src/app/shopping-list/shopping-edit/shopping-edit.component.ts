5c1b5093d6ad00433d71bed643a8a8922bf266eb
Matheus <matheuspq79@outlook.com.br>
2017-12-21 15:45:02 -0200

    Acrescentar ingredientes na shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+// import { Ingredient } from '../../shared/ingredient.model';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem(){ 
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingamount = this.amountInputRef.nativeElement.value; 
+    //Não pretende mudar os nomes das variáveis uma vez declaradas, por isso são const.
+    //São criadas dentro do escopo deste método td vez que ele é executado.. (eu acho :P)
+
+    const newIngredient = new Ingredient(ingName, ingamount);
+    
+    this.ingredientAdded.emit({
+      name: this.nameInputRef.nativeElement.value,
+      amount: this.amountInputRef.nativeElement.value
+    });
   }
 
 }
